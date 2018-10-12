c684547dcfa2729219489b3ab6619f3a2a8633f8
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-10-12 11:52:36 -0700

    Updated command shell script

--- imp-commands.sh
+++ imp-commands.sh
@@ -1,19 +1,23 @@
 #----------------------------------
-# Generate SSL cert; Make sure to substitute 'localhost' with the DNS label for the microservice!
+# Author: Ganesh Radhakrishnan (ganrad01@gmail.com) @ Microsoft
+# Dated: 10-12-2018
+# Description:
+# Generate SSL cert for securing the po-service microservice.
+#----------------------------------
+#
+#----------------------------------
+# Specify correct value for host in '-subj'!!
+# Substitute 'localhost' with DNS name of deployed microservice!
 #----------------------------------
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj '/CN=po-service.033185fc7e8b483fae46.westus.aksapp.io'
 
 # Print certificate and verify subject value; should point to the app dns hostname
 openssl x509 -in tls.crt -text -noout
 
-# Base64 encode the key and cert
-#echo key.pem | base64 > app.key
-#echo cert.pem | base64 > app.crt
 #----------------------------------
-# Create the secret 'ssh-key-secret'
+# Create the secret 'po-ssh-secret' of TYPE = tls!!
 # This secret has to be created in the k8s namespace before deploying the application.
 #----------------------------------
-#kubectl create secret generic ssh-key-secret --from-file=tls.key=./app.key --from-file=tls.crt=./app.crt
 kubectl create secret tls po-ssh-secret --key tls.key --cert tls.crt
 
 # Verify the secret and it's contents
@@ -25,6 +29,16 @@ kubectl get secret po-ssh-secret -o yaml
 # Run the application build in Azure DevOps
 #
 #----------------------------------
+# After microservices are deployed, use these commands to inspect SSL config, ingress resources etc
+#----------------------------------
+# Verify TLSv1.2/SSL handshake using curl
+curl -v -k https://po-service.033185fc7e8b483fae46.westus.aksapp.io/
+# View the deployed ingress resource
+kubectl get ingress po-service -o yaml
+#----------------------------------
+
+
+
 #----------------------------------
 # Tear down po-service and mongodb apps
 #---------------------------------
