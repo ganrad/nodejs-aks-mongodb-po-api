f2f1184c982dde1f46dcecd7c99769efe6c18986
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-10-12 12:06:40 -0700

    Updated test shell scripts

--- test-scripts/delete-order.sh
+++ test-scripts/delete-order.sh
@@ -1,1 +1,1 @@
-curl -i -X POST http://localhost:8080/orders/5bbe506d7cb35e1a6aeeb0ca/delete
+curl -v -k -X POST https://po-service.033185fc7e8b483fae46.westus.aksapp.io/orders/5bbe506d7cb35e1a6aeeb0ca/delete
